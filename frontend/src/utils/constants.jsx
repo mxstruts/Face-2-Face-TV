cb6fa82252efb8c3c60636c54f04d562c0eb15c4
mxstruts <shatilo.max@gmail.com>
2024-07-18 23:13:14 -0400

    changed constants

--- frontend/src/utils/constants.jsx
+++ frontend/src/utils/constants.jsx
@@ -1,3 +1,3 @@
 // export const backend_url = 'https://backend-production-dd16.up.railway.app'
-// export const backend_url = 'https://face-2-face-tv-server.vercel.app'
-export const backend_url = 'http://localhost:8800'
+export const backend_url = 'https://face-2-face-tv-server.vercel.app'
+// export const backend_url = 'http://localhost:8800'
